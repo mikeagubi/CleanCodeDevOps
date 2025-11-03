a5cdb6608d60919e92b3e6350d875bfe5ce1a8c6
Michael Agubi <mike.1985@live.se>
2025-11-03 22:46:14 +0100

    Added Hello message

---
+++ CleanCodeDevOps/CalculatorApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CalculatorApp;
+
+Console.WriteLine("Hello, World!");
+var calc = new Calculator();
+Console.WriteLine("sum:" +calc.Subtract(3,2));
