c6e8b30fc8bbe0ea471eaa44d28f7d2f5c618619
Michael Agubi <mike.1985@live.se>
2025-11-05 00:41:47 +0100

    Deleted Calculate App

--- CleanCodeDevOps/CalculatorApp/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using CalculatorApp;
-
-Console.WriteLine("Hello, World!");
-var calc = new Calculator();
-Console.WriteLine("sum:" +calc.Subtract(3,2));
