ee5471be9d92861e55dd2bcc628f1389792e0c3f
Michael Agubi <mike.1985@live.se>
2025-11-05 02:28:01 +0100

    DeterministiskKlass

--- CleanCodeDevOps/CalculatorApp/Calculator.cs
+++ CleanCodeDevOps/CalculatorApp/Calculator.cs
@@ -1,32 +1,21 @@
+using System.Globalization;
+
 namespace CalculatorApp;
 
 public class Calculator
 {
-    public void Add(int a, int b)
-    {
-        Console.WriteLine($"Adding {a} + {b}");
-    }
-    
-    public void Subtract(int a, int b)
-    {
-        Log($"Subtracting {a} - {b}");
-    }
-    
-    public void Multiply(int a, int b)
-    {
-        if (a == 0 || b == 0)
-        {
-            throw new InvalidOperationException("Multiplication by zero is not allowed.");
-        }
-    }
+    public int Add(int a, int b) => a + b;
+
+    public int Subtract(int a, int b) => a - b;
+
+    public double Multiply(int a, int b) => a * b;
 
 
-    public void Divide(int a, int b)
+    public double Divide(int a, int b)
     {
         if (b == 0)
-        {
-            Console.WriteLine("Division by zero attempted");
-        }
+            throw new DivideByZeroException();
+        return (double)a / b;
     }
     
     
--- CleanCodeDevOps/CalculatorApp/Program.cs
+++ CleanCodeDevOps/CalculatorApp/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using CalculatorApp;
+
 Console.WriteLine("Hello, World!");
+
+var calc = new Calculator();
+
+Console.WriteLine(calc.Divide(4, 0));
