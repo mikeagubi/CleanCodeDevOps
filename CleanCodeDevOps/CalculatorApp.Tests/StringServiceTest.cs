a32d0a20a6fccff18e38284bd531dfd8e766acee
Michael Agubi <mike.1985@live.se>
2025-11-08 20:53:17 +0100

    AddedRegexTest

--- CleanCodeDevOps/CalculatorApp.Tests/StringServiceTest.cs
+++ CleanCodeDevOps/CalculatorApp.Tests/StringServiceTest.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace CalculatorApp.Tests;
 
 public class StringServiceTest
@@ -18,6 +20,31 @@ public class StringServiceTest
         string expected = "test";
         var actual = sut.Echo("TEST");
         Assert.Equal(expected, actual, ignoreCase: true);
+    }
+
+    [Fact]
+    public void Greet_ContainsHello()
+    {
+        var sut = new StringService();
+        string actual = sut.Greet();
+        Assert.Contains("Hello", actual);
+        
+    }
+
+    [Fact]
+    public void Greet_EndsWithWord()
+    {
+        StringService sut = new();
+        var actual = sut.Greet();
+        Assert.EndsWith("d", actual);
+    }
+
+    [Fact]
+    public void Echo_MatchesRegex()
+    {
+        var sut = new StringService();
+        string actual = sut.Echo("Test123");
+        Assert.Matches(@"^[A-Za-z]{4}[1-9]{3}$", actual);
     }
     
     
